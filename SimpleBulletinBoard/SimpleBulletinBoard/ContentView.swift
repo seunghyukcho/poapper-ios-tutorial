50209f1c7585c3f1341bbb7b47e5911d06a11748
seunghyek.cho <shhj1998@postech.ac.kr>
2019-11-04 14:10:54 +0900

    created base project

---
+++ SimpleBulletinBoard/SimpleBulletinBoard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleBulletinBoard
+//
+//  Created by 조승혁 on 2019/11/04.
+//  Copyright © 2019 조승혁. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
